f0a60b66bdb131c07080997bd917f9a74174f524
Mehdi Salimi <salimimehdi@yahoo.com>
2023-06-11 03:14:13 +0200

    hh

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+const btn_new=document.getElementById("btn_new");
+
+btn_new.addEventListener("click",function(){
+    window.open("../form.html", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=500,left=500,width=900,height=400");
+});
